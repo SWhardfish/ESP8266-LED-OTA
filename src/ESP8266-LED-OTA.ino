bba88a25e51032a0c22c762118d21ed1ff6d4717
aryl <arylander@gmail.com>
2025-02-23 12:59:00 +0100

    v1.0.16

--- src/ESP8266-LED-OTA.ino
+++ src/ESP8266-LED-OTA.ino
@@ -17,7 +17,7 @@
 #define SWITCH_PIN D5      // The ESP8266 pin connected to the momentary switch
 #define STATUS_LED D4      // Status LED for WiFi connection feedback
 
-const String current_version = "1.0.3";  // Set this to the current version of your firmware
+const String current_version = "1.0.16";  // Set this to the current version of your firmware
 const String api_url = "https://api.github.com/repos/SWhardfish/ESP8266-LED-OTA/releases/latest"; // GitHub API for latest release
 const char *firmware_url = "https://github.com/SWhardfish/ESP8266-LED-OTA/releases/latest/download/firmware.bin"; // URL to firmware binary
 
@@ -214,7 +214,7 @@ String getHTML() {
     html += ".button{padding:10px 20px;font-size:18px;display:inline-block;margin:10px;border:none;background:blue;color:white;cursor:pointer;}";
     html += "</style></head><body>";
 
-    html += "<h2>ESP8266 Web Server WITH OTA ***v1.0.6</h2>";
+    html += "<h2>ESP8266 Web Server WITH OTA v1.0.16</h2>";
     html += "<p>LED state: <strong style='color: red;'>";
     html += (LED_state == LOW) ? "OFF" : "ON";
     html += "</strong></p>";
